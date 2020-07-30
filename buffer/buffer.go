8ed1382823b9c94eab1c6ff7c62c0997af576e3f
Benjamim <benjamim.francisco@hotmail.com>
2020-07-30 19:57:27 -0300

    buffers

---
+++ buffer/buffer.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//entendendo funcionamento do buffer de um canal
+
+func rotina(canal chan int) {
+	canal <- 1
+	canal <- 2
+	fmt.Println("Executou")
+	canal <- 3
+	canal <- 4
+}
+
+func main() {
+	// declarando um canal com buffer de 3 posicoes
+	canal := make(chan int, 3)
+	go rotina(canal)
+	//lendo o buffer
+	fmt.Println(<-canal)
+}
+
+//como o buffer possui 3 posicoes ele ficará bloqueado até que se cosuma o dado de uma das posicoes p/ assim um novo dado ser inserido
